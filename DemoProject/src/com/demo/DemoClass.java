1557b6aa71166387e2b00b41cf9ef0d50cc4d94b
Diwahar.Sundar <diwahar.sundar@gmail.com>
2018-06-09 17:10:32 +0530

    First Commit

--- DemoProject/src/com/demo/DemoClass.java
+++ DemoProject/src/com/demo/DemoClass.java
@@ -3,13 +3,10 @@ package com.demo;
 public class DemoClass {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Working");
-		
-		
+		System.out.println("Yes!! Yes!! Yes!!");
+
 	}
-	
-	
-	
-	
+
 }
